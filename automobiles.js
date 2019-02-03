04d0c23c8452aa473a9e3864a4e0f4168f11a6e7
johnwalt8 <46585029+johnwalt8@users.noreply.github.com>
2019-02-03 15:15:23 -0800

    assignment complete, automobiles.js to turn in

--- automobiles.js
+++ automobiles.js
@@ -1,12 +1,6 @@
 // Walter Johnson
 // Week 4 HW Assignment: Higher-Order Functions and Objects
 
-//This assignment is graded based on correctness and will require you to use higher-order functions to sort automobiles.
-//The description is below and can also be found here  (Links to an external site.)Links to an external site.on jsFiddle.
-//You should submit a single .js file called automobile.js which when run with node.js using the command "node automobile.js" produces the described results.
-//You must make use of  higher-order functions to sort the cars. You should not, for example, create entirely separate functions each with dedicated loops to sort the cars.
-//You will need a loop (or potentially more than one loop depending on your sorting algorithm of choice) in the sortArr function but that is pretty much it. Use prototype whenever needed.
-
 const X = Number.MIN_SAFE_INTEGER;
 
 function Automobile(year, make, model, type) {
@@ -56,7 +50,7 @@ function sortArr(comparator, array) {
     }
 }
 
-// compares integers
+// compares integers, used by other comparators
 function exComparator(int1, int2) {
     if (int1 > int2){
         return true;
@@ -71,23 +65,11 @@ function yearComparator(auto1, auto2) {
      return exComparator(auto1.year, auto2.year || X);
 }
 
-var logFunc = function (item) {
-    item.logMe(true);
-}
-
-console.log("");
-automobiles.forEach(logFunc);
-
 // This compares two automobiles based on their make.
 // It should be case insensitive and makes which are alphabetically earlier in the alphabet are "greater" than ones that come later.
 function makeComparator(auto1, auto2) {
     return auto1.make.localeCompare(auto2.make) === -1;
 }
-
-sortArr(makeComparator, automobiles);
-
-console.log("");
-automobiles.forEach(logFunc);
 
 // This compares two automobiles based on their type.
 // The ordering from "greatest" to "least" is as follows: roadster, pickup, suv, wagon, (types not otherwise listed).
@@ -103,38 +85,6 @@ function typeComparator(auto1, auto2) {
     }
 }
 
-sortArr(typeComparator, automobiles);
-
-console.log("");
-automobiles.forEach(logFunc);
-
-// Your program should output the following to the console.log, including the opening and closing 5 stars. 
-// All values in parenthesis should be replaced with appropriate values. Each line is a seperate call to console.log.
-
-// Each line representing a car should be produced via a logMe function.
-// This function should be added to the Automobile class and accept a single boolean argument.
-// If the argument is 'true' then it prints "year make model type" with the year, make, model and type being the values appropriate for the automobile.
-// If the argument is 'false' then the type is ommited and just the "year make model" is logged.
-
-// *****
-// The cars sorted by year are:
-// (year make model of the 'greatest' car)
-// (...)
-// (year make model of the 'least' car)
-
-// The cars sorted by make are:
-// (year make model of the 'greatest' car)
-// (...)
-// (year make model of the 'least' car)
-
-// The cars sorted by type are:
-// (year make model type of the 'greatest' car)
-// (...)
-// (year make model type of the 'least' car)
-// *****
-
-// As an example of the content in the parenthesis:
-// 1990 Ford F-150
 console.log("");
 console.log("*****");
 console.log("The cars sorted by year are:");
